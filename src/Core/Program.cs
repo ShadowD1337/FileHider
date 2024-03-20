ec6d05bc46e3c7b28faaa11f9fa2c6f7cf345f8d
ShadowD1337 <121127640+ShadowD1337@users.noreply.github.com>
2024-03-20 08:56:13 +0200

    Made project and an example usage of the StegoSharp library

---
+++ src/Core/Program.cs
@@ -0,0 +1,15 @@
+﻿using StegoSharp;
+using StegoSharp.Enums;
+using System.Drawing;
+using System.Text;
+
+namespace Stego
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ src/StegoExample/StegoExample.cs
@@ -0,0 +1,24 @@
+﻿using StegoSharp;
+using StegoSharp.Enums;
+using System.Drawing;
+using System.Text;
+
+namespace Stego
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var image = new StegoImage("C:\\Users\\user\\Desktop\\old.jpg");
+            image.Strategy.ColorChannels = new[] { ColorChannel.G, ColorChannel.B, ColorChannel.R };
+            image.Strategy.BitsPerChannel = 2;
+
+            var msg = "test123";
+            image.EmbedPayload(msg);
+            image.Save("C:\\Users\\user\\Desktop\\new.jpg");
+
+            Console.WriteLine(Encoding.Default.GetString(image.ExtractBytes().ToArray()).Substring(0, msg.Length));
+            Console.WriteLine(image.ByteCapacity);
+        }
+    }
+}
